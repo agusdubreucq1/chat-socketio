bdd6751bde862ac854271900d386b3668846f9cf
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2024-03-15 16:02:48 -0300

    header

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -2,21 +2,31 @@ import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LogoutButton from '../common/LogoutButton';
+import { UserIcon } from '../common/icons/UserIcon';
 
 const Header: React.FC = () => {
     const { loginWithRedirect, isAuthenticated, user } = useAuth0()
 
     return (
         <header className='sticky top-0 z-40 flex bg-slate-900 items-center p-4 justify-center'>
-            <nav className='flex gap-4 max-w-[1200px] justify-start w-full'>
+            <nav className='flex gap-4 max-w-[1200px] px-4 justify-between items-center w-full'>
                 {
                     isAuthenticated
                         ?
                         <>
-                            <Link className='text-xl text-white' to={'/profile'}>Profile</Link>
-                            <Link className='text-xl text-white' to={'/'}>Chats</Link>
-                            <LogoutButton></LogoutButton>
-                            <p className='text-xl text-white'>{user?.name}</p>
+                            <div role='rutas' className='flex'>
+                                <Link className='text-xl text-white hover:text-gray-400' to={'/'}>
+                                    Chats
+                                    </Link>
+                            </div>
+                            <div role='auth' className='flex gap-4'>
+                                <Link className='flex items-center gap-1 text-nowrap text-xl w-max text-white hover:text-gray-400' to={'/profile'}>
+                                    <UserIcon className='w-10 h-10' />
+                                    {user?.name}
+                                </Link>
+                                <LogoutButton></LogoutButton>
+                            </div>
+
                         </>
                         :
                         <>
