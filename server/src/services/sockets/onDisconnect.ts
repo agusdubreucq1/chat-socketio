79d2341c2398b775ae49236a92b84a9a5f7b9fd9
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2024-03-18 19:50:08 -0300

    error de causa bucle infinito

--- server/src/services/sockets/onDisconnect.ts
+++ server/src/services/sockets/onDisconnect.ts
@@ -1,18 +1,19 @@
 import { Server, Socket } from 'socket.io'
-import { UserInfo, UserOnline } from '../../types'
+import { UserInfo} from '../../types'
 
 interface Params {
   user: UserInfo
   socket: Socket
   io: Server
-  getOnlineUsers: () => UserOnline[]
-  setOnlineUsers: (users: UserOnline[]) => void
+  getOnlineUsers: () => UserInfo[]
+  setOnlineUsers: (users: UserInfo[]) => void
 }
 
-export const addToOnlineUsers = ({ user, io,socket, getOnlineUsers, setOnlineUsers }: Params) => {
+export const addToOnlineUsers = ({ user, io, socket, getOnlineUsers, setOnlineUsers }: Params) => {
   const onlineUsers = getOnlineUsers()
+  console.log(socket.id)
   if (!onlineUsers.find((u) => u.sub === user.sub)) {
-    const newUser = { ...user, socket: socket }
+    const newUser = user//{ ...user, socket}
     const newOnlineUsers = [...onlineUsers, newUser]
     setOnlineUsers(newOnlineUsers)
     console.log('new user online', user.name)
