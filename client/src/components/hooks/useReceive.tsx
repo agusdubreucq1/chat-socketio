2a9961ba051330c4ddfd5c050ece938a2c66d7cf
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2024-03-18 15:19:41 -0300

    unReadMessage and update chats

--- client/src/components/hooks/useReceive.tsx
+++ client/src/components/hooks/useReceive.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useSocket } from '../../globalState/socket';
 import { useQueryClient } from '@tanstack/react-query';
-import { MessageTypeResponse } from '../../vite-env';
+import { ChatTypeResponse, MessageTypeResponse } from '../../vite-env';
+import { useUnreadMessages } from '../../globalState/unreadMessages';
 
 const useReceive = () => {
     const socket = useSocket((state) => state.socket);
     const client = useQueryClient();
     const token = client.getQueryData(['token']) as string
+    const addUnreadMessage = useUnreadMessages(state => state.addUnreadMessage)
 
     useEffect(() => {
         if (socket && token && client) {
@@ -15,9 +17,22 @@ const useReceive = () => {
                 client.setQueryData(['messages', newMsg.chat_id, token], (old: MessageTypeResponse[]) => {
                     return [...old, newMsg]
                 })
+                client.setQueryData(['chats', token], (old: ChatTypeResponse[]) => {
+                    return old.map(chat => {
+                        if (chat.id === newMsg.chat_id) {
+                            return {
+                                ...chat,
+                                message: newMsg,
+                                last_message_id: newMsg.id
+                            }
+                        }
+                        return chat
+                    })
+                })
+                addUnreadMessage(newMsg)
             })
-        }
-    }, [socket, client, token])
+        }//TODO: receive all msg in a community global state 
+    }, [socket, client, token, addUnreadMessage])
 
 };
 
