269cdf04e94a152ad541221007335664225291c4
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2024-03-28 15:18:24 -0300

    connection db always data

--- server/src/models/sequelize/conection.ts
+++ server/src/models/sequelize/conection.ts
@@ -1,7 +1,16 @@
 import { Sequelize } from 'sequelize'
+import { config } from 'dotenv'
 
-export const db = new Sequelize('chat-socketio', 'root', '', {
+config()
+
+const db_name = process.env.DB_NAME || ''
+const db_user = process.env.DB_USER || 'root'
+const db_password = process.env.DB_PASSWORD || ''
+const db_host = process.env.DB_HOST || 'localhost'
+
+export const db = new Sequelize(db_name, db_user, db_password, {
   dialect: 'mysql',
-  host: 'localhost',
+  dialectModule: require('mysql2'),
+  host: db_host,
   logging: false,
 })
